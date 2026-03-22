added1acaa79eab2d9e29e75aeb434c116cbcf35
zen7s <reynal@mail.ru>
2026-03-22 14:47:21 +0300

    feature: added fade when scrolling for CompanyName component

--- src/pages/MainPage/components/Hero/components/CompanyName.tsx
+++ src/pages/MainPage/components/Hero/components/CompanyName.tsx
@@ -5,8 +5,9 @@ import { useGSAP } from '@gsap/react'
 import { gsap } from '@shared/lib/gsap'
 
 export const CompanyName = ({ title, subtitle }: { title: string; subtitle: string }) => {
-  const titleRef = useRef(null)
-  const subtitleRef = useRef(null)
+  const container = useRef<HTMLDivElement | null>(null)
+  const titleRef = useRef<HTMLHeadingElement | null>(null)
+  const subtitleRef = useRef<HTMLParagraphElement | null>(null)
 
   const letters = title.split('').map((letter, i) => (
     <span key={i} className="letter inline-block">
@@ -14,32 +15,75 @@ export const CompanyName = ({ title, subtitle }: { title: string; subtitle: stri
     </span>
   ))
 
-  useGSAP(() => {
-    gsap.fromTo(
-      '.letter',
-      { opacity: 0, y: 100, rotationX: -90 },
-      {
-        opacity: 1,
-        y: 0,
-        rotationX: 0,
-        duration: 1,
-        stagger: 0.05,
-        ease: 'back.out(1.7)',
-        delay: 0.2,
-      },
-    )
+  useGSAP(
+    () => {
+      if (!titleRef.current) return
+      const lettersEl = titleRef.current?.querySelectorAll('.letter')
 
-    if (subtitleRef.current) {
+      gsap.fromTo(
+        lettersEl,
+        { opacity: 0, y: 100, rotationX: -90 },
+        {
+          opacity: 1,
+          y: 0,
+          rotationX: 0,
+          duration: 1,
+          stagger: 0.05,
+          ease: 'back.out(1.7)',
+          delay: 0.2,
+        },
+      )
+
       gsap.fromTo(
         subtitleRef.current,
         { opacity: 0, y: 50 },
-        { opacity: 1, y: 0, duration: 1, delay: 1.2, ease: 'power3.out' },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          delay: 1.2,
+          ease: 'power3.out',
+        },
       )
-    }
-  })
+
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: container.current,
+          start: 'top 40%',
+          end: 'bottom top',
+          scrub: true,
+        },
+      })
+
+      tl.to(
+        titleRef.current,
+        {
+          y: -200,
+          opacity: 0,
+          scale: 0.8,
+          filter: 'blur(10px)',
+          ease: 'none',
+        },
+        0,
+      )
+
+      tl.to(
+        subtitleRef.current,
+        {
+          y: -100,
+          opacity: 0,
+          scale: 0.9,
+          filter: 'blur(6px)',
+          ease: 'none',
+        },
+        0,
+      )
+    },
+    { scope: container },
+  )
 
   return (
-    <div className="text-center">
+    <div ref={container} className="text-center">
       <h1 ref={titleRef} className="text-9xl">
         {letters}
       </h1>
